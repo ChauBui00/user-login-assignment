07d19759a3c6076c07659a5c1aab1eb533400934
ChauBui00 <c.bui3@share.epsb.ca>
2022-10-27 09:48:52 -0600

    update

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+    // Input
+    let name = document.getElementById("username").value.toLowerCase();
+    let pass = document.getElementById("pass").value;
+
+    console.log(name)
+    console.log(pass)
+    // If Statement - Test the Input
+    
+    if (name === "admin" && pass === "1234") {
+     alert("Login Successful");  
+    } else if (name === "----") {
+        alert("Invalid Username");
+    } else{
+        alert("Invlid Password");
+    }
+    
+}
