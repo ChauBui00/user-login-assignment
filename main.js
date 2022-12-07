04ba258a4f4f76c5d2690dc75f7689dbf55f95ba
ChauBui00 <c.bui3@share.epsb.ca>
2022-12-07 10:28:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-
-
-
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-    // Input
-    let name = document.getElementById("username").value.toLowerCase();
-    let pass = document.getElementById("pass").value;
+  // Input
+  let name = document.getElementById("username").value.toLowerCase();
+  let pass = document.getElementById("pass").value;
 
-    console.log(name)
-    console.log(pass)
-    // If Statement - Test the Input
-    
-    if (name === "admin" && pass === "1234") {
-     alert("Login Successful");  
-    } else if (name === "----") {
-        alert("Invalid Username");
-    } else{
-        alert("Invlid Password");
-    }
-    
+  console.log(name);
+  console.log(pass);
+  // If Statement - Test the Input
+
+  if (name === "admin" && pass === "1234") {
+    alert("Login Successful");
+  } else if (name != "admin") {
+    alert("Invalid Username");
+  } else {
+    alert("Invlid Password");
+  }
 }
